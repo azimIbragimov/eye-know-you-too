08e09837765c31c373a68102c5c0e043414006a5
azimibragimov <a.ibragimov@login9.ufhpc>
2024-04-24 19:15:46 -0400

    fixes

--- train.sh
+++ train.sh
@@ -1,20 +1,3 @@
-#!/bin/bash
-#SBATCH --job-name=ekyt
-#SBATCH --output=ekyt-cnn.out
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=a.ibragimov@ufl.edu
-#SBATCH --ntasks=1
-#SBATCH --mem=16gb
-#SBATCH --partition=gpu
-#SBATCH --gpus=a100:1
-#SBATCH --time=48:00:00
-
-source ~/.bashrc
-conda activate /blue/cai4104/a.ibragimov/.conda/ekyt-release-new-new
-echo hello
-
-cd /blue/cai4104/a.ibragimov/ew/ekyt/eye-know-you-too
-
 # Training EKYT model across every ds value
 CUDA_VISIBLE_DEVICES=0 python src/train.py --ds=1 --fold=0
 CUDA_VISIBLE_DEVICES=0 python src/train.py --ds=1 --fold=1
